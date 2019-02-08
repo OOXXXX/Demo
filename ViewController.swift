69944d4d28278c67ce5e4b23c5d9d774eb3ecc3c
Patrick <46059506+OOXXXX@users.noreply.github.com>
2019-02-08 14:04:22 +0800

    Add files via upload

---
+++ ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  Demo
+//
+//  Created by Rhapsody on 2018/12/31.
+//  Copyright © 2018 Back to Bedlam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        let img = UIImage(named: "firework1")
+        let imgview = UIImageView(image: img)
+        self.view.addSubview(imgview)
+        
+        let rect = CGRect(x: 55, y: 60, width: 360, height: 90)
+        let label = UILabel(frame: rect)
+        label.text = "Happy New Year !"
+        
+        let font = UIFont(name: "Arial", size: 38)
+        label.font = font
+        label.textColor = UIColor.white
+        
+        self.view.addSubview(label)
+    }
+
+}
+
