146b27823f6510d98f5609eb73748c19fde273b9
Patrick <46059506+OOXXXX@users.noreply.github.com>
2019-02-13 23:34:03 +0800

    Update DemoTests.swift

--- DemoTests.swift
+++ DemoTests.swift
@@ -9,17 +9,21 @@
 import XCTest
 @testable import Demo
 
-class DemoTests: XCTestCase {
+class DemoTests: XCTestCase
+{
 
-    override func setUp() {
+    override func setUp() 
+    {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
 
-    override func tearDown() {
+    override func tearDown()
+    {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() {
+    func testExample() 
+    {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
